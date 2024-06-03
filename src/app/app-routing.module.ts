bb007cbb412e28af314d26b71b74a6bfe25c9d07
git-sravya <s.sravya@yahoo.com>
2024-06-03 15:02:57 +0530

    Added route and Remove cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'App',pathMatch:'full'},  
+  {path:'Product',component:ProductComponent},
+  // {path:'Login',component:LoginComponent},
+  // {path:'**',component:ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
